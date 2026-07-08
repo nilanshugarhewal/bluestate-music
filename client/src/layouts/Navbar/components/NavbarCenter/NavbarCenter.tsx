dcaf10d2417db5faed2245a7f0bea012a5ef6a55
Nilanshu Garhewal <nilanshugarhewal1@gmail.com>
2026-07-08 16:11:46 +0530

    /client: Fixed navbar responsiveness.

---
+++ client/src/layouts/Navbar/components/NavbarCenter/NavbarCenter.tsx
@@ -0,0 +1,39 @@
+import { Link, useLocation } from "react-router-dom";
+import "./NavbarCenter.scss";
+
+const NavLinks = () => {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => {
+    if (path === "/") return pathname === "/";
+    return pathname.startsWith(path);
+  };
+
+  return (
+    <div className="navbar-center">
+      <div className="navbar-group">
+        <div className="nav-links">
+          <Link
+            to={"/home"}
+            className={`icon uni-link ${isActive("/home") ? "active" : ""}`}
+          >
+            <span className="icon-child">
+              <span>Home</span>
+            </span>
+          </Link>
+
+          <Link
+            to={"/tracks"}
+            className={`icon uni-link ${isActive("/track") ? "active" : ""}`}
+          >
+            <span className="icon-child">
+              <span>All Beats</span>
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavLinks;
---
+++ client/src/layouts/Navbar/components/NavbarLeft/NavbarLeft.tsx
@@ -0,0 +1,12 @@
+import "./NavbarLeft.scss"
+
+const NavbarLeft = () => {
+    return (
+        <div className="navbar-left">
+            {/* <img className="bluestate-logo" src="/assets/logo/bluestate_long_logo.png" alt="logo" /> */}
+            <p>BLUESTATE</p>
+        </div >
+    )
+}
+
+export default NavbarLeft;
